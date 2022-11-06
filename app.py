d9f6dab6197f649ad28d4dd2f70bbe0c30d0c6aa
jccramos <ramosjulio771@gmail.com>
2022-11-06 09:14:07 -0300

    Enable request via curl

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from crawler import main
 
 
 app = Flask(__name__)
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/anotai', methods=['GET', 'POST'])
 def welcome():
-    mock = "https://sat.sef.sc.gov.br/nfce/consulta?p=42220805116621000310650070000025731331831472|2|1|1|FF9A732669D99F2CBC85F5D09E4F0E1EEBD3EA9B"
-    item, local = main(mock)
-    json_item = item.to_json()
-    local_item =local.to_json()
-    return json_item
+    if request.method == "POST":
+        url = request.get_json(force=True)["url"]
+        breakpoint()
+        item, local = main(url)
+        json_item = item.to_json()
+        # TODO: Wait the implementation in mobile to return this one!
+        #local_item =local.to_json()
+        return json_item
+    else:
+        return "The request method was unexpected!!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
